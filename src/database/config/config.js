eecf5f6e1217ea10a0fdd54354cd2379a8aa277a
pgianferro <pablogianferro@gmail.com>
2023-02-15 20:22:01 -0300

    more fixes

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,19 +6,20 @@ const {DB_MYSQLPORT} = require('../../../config')
 
 module.exports = {
   "development": {
-    "username": 'root',
-    "password": '',
-    "database": "coleccionables_db",
-    "host": "127.0.0.1",
-    "port":8889,
+    "username": DB_MYSQLUSER,
+    "password": DB_MYSQLPASSWORD,
+    "database": DB_DATABASE,
+    "port": DB_MYSQLPORT,
+    "host": DB_MYSQLHOST,
     "dialect": "mysql",
     "logging": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": DB_MYSQLUSER,
+    "password": DB_MYSQLPASSWORD,
+    "database": DB_DATABASE,
+    "port": DB_MYSQLPORT,
+    "host": DB_MYSQLHOST,
     "dialect": "mysql"
   },
   "production": {
