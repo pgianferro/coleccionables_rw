c09aace457022b43bfc5beee0f1505d1f59ccae6
pgianferro <pablogianferro@gmail.com>
2023-02-18 16:44:02 -0300

    more fixes

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.use('/usuarios', userRouter);
 app.use('/api', apiRouter);
 
 // Inicio Servidor
-app.listen(PORT, () => {
+app.listen(`0.0.0.0:$PORT`, () => {
     console.log(`Up & Running en puerto ${PORT}`);
 });
